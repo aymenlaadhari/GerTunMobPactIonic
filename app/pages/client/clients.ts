84875d234c5c3d7e4fa7fed291248dc4b936d4bb
Aymen <aymenlaadhari@gmail.com>
2016-10-25 09:00:06 +0200

    updating client list

--- app/pages/client/clients.ts
+++ app/pages/client/clients.ts
@@ -1,25 +1,27 @@
 import {Component} from '@angular/core';
-import {NavController,Loading} from 'ionic-angular';
+import {NavController, Loading} from 'ionic-angular';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {ClientDetails} from '../client/clientDetails';
 import {Clients} from '../model/client';
 
 @Component({
-  templateUrl: 'build/pages/client/clientList.html'
+    templateUrl: 'build/pages/client/clientList.html'
 })
 export class Client {
     clientsFeed: Array<any>;
     clients: Array<Clients>;
-    newClient:Clients;
+    filteredClients: Array<Clients>;
+    newClient: Clients;
 
-  constructor(private navCtrl: NavController,private http: Http) {
+    constructor(private navCtrl: NavController, private http: Http) {
 
-      this.clients = [];
-      this.presentLoading();
-
-  }
-   public presentLoading() {
+        this.clients = [];
+        this.filteredClients = [];
+        this.presentLoading();
+        this.getClientByCountries('ALG');
+    }
+    public presentLoading() {
         let loading = Loading.create({
             content: "Please wait...",
             duration: 3000
@@ -27,28 +29,29 @@ export class Client {
         this.navCtrl.present(loading);
         this.loadData();
     }
-    
+
     loadData() {
         this.http.get('https://newmobpact.firebaseapp.com/img/clients.json').map(res => res.json()).subscribe(data => {
             // TypeScript
             this.clientsFeed = data.Clients;
-
             for (let i = 0; i < this.clientsFeed.length; i++) {
-                this.newClient = new Clients(this.clientsFeed[i].name, this.clientsFeed[i].description, this.clientsFeed[i].image,this.clientsFeed[i].country,this.clientsFeed[i].products, this.clientsFeed[i].revenues);
+                this.newClient = new Clients(this.clientsFeed[i].name, this.clientsFeed[i].description, this.clientsFeed[i].image, this.clientsFeed[i].country, this.clientsFeed[i].products, this.clientsFeed[i].revenues);
                 this.clients.push(this.newClient);
                 console.log(this.clients[i]);
             }
             console.log(this.clients.length);
-            //this.loadGrid(); 
+            
         });
+    }
 
+
+    public openClientDetails(client: Clients) {
+        this.navCtrl.push(ClientDetails, { client: client });
+    }
+
+    public getClientByCountries(country: string) {
+        //this.filteredClients = this.clients.filter((client: Clients) => client.country == country);
+        this.filteredClients = this.clients.filter((client) => client.country === country );
         
     }
-    
-  
-  public openClientDetails(client:Clients)
-{
-    this.navCtrl.push(ClientDetails,{ client: client});
-}
-
 }
